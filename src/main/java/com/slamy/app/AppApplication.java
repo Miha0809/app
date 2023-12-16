b7f42981bbfcb0ff0c481703134202835819389c
Mark <miha080104@gmail.com>
2023-12-16 23:41:40 +0100

    Change in main file

--- src/main/java/com/slamy/app/AppApplication.java
+++ src/main/java/com/slamy/app/AppApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);}
+	public static void main(String[] args) {SpringApplication.run(AppApplication.class, args);}
 	// TODO: make a ERD diagram
 }
